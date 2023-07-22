9ef7d4283640946c30a126e9a78a9c9b230dc1ba
Reza paidar <reza.paidar@outlook.com>
2023-07-22 07:11:40 +0330

    Create SubarrayDivision.cs

---
+++ SubarrayDivision.cs
@@ -0,0 +1,80 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<int> s, int d, int m)
+    {
+        int count = 0;
+
+        // If the number of squares is less than the birth month, there is no valid way to divide the chocolate
+        if (s.Count < m)
+            return count;
+
+        // Loop through the list of integers to find segments that meet the criteria
+        for (int i = 0; i <= s.Count - m; i++)
+        {
+            int segmentSum = 0;
+
+            // Calculate the sum of the segment of length m
+            for (int j = i; j < i + m; j++)
+            {
+                segmentSum += s[j];
+            }
+
+            // If the sum of the segment matches Ron's birth day, increment the count
+            if (segmentSum == d)
+                count++;
+        }
+
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
